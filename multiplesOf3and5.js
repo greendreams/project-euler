fa095c6fd19deccaa2c626792974d689fad402ae
Karthik <springcoding@gmail.com>
2020-03-14 13:09:06 -0700

    Minor fix

--- multiplesOf3and5.js
+++ multiplesOf3and5.js
@@ -3,6 +3,7 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 let result = 0;
+
 function multiplesOf3and5(number) {
   let values = [];
   for(let i=0; i<number; i++){
@@ -10,11 +11,11 @@ function multiplesOf3and5(number) {
       values.push(i)
     }
   }
-  values.forEach(addEm)
+  values.forEach(addValues)
   console.log(result)
 }
 
-function addEm(value, index, arr) {
+function addValues(value) {
   result = result + value;
 }
 
